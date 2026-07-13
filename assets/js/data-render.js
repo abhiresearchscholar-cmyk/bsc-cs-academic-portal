426c81fe8f8cd427d42017e1bf91fc06cb2c584d
abhinmodgil <aasvi2006@gmail.com>
2026-07-13 06:45:27 +0530

    Add lecture five notes and lecture six

--- assets/js/data-render.js
+++ assets/js/data-render.js
@@ -1,6 +1,6 @@
 async function loadJson(path) {
   const separator = path.includes("?") ? "&" : "?";
-  const response = await fetch(path + separator + "v=20260710-lecture05");
+  const response = await fetch(path + separator + "v=20260713-lecture06");
   if (!response.ok) {
     throw new Error("Could not load " + path);
   }
