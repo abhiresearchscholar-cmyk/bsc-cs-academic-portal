1ada461bc9ec6ad4b74ad5212a6d88c26b4b4514
abhinmodgil <aasvi2006@gmail.com>
2026-07-13 07:14:57 +0530

    Add lecture seven materials

--- assets/js/data-render.js
+++ assets/js/data-render.js
@@ -1,6 +1,6 @@
 async function loadJson(path) {
   const separator = path.includes("?") ? "&" : "?";
-  const response = await fetch(path + separator + "v=20260713-lecture06");
+  const response = await fetch(path + separator + "v=20260713-lecture07");
   if (!response.ok) {
     throw new Error("Could not load " + path);
   }
